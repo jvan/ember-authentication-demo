98e799c990c6a9e7449fa277f6bea4dcd82370b2
jvan <jvan@cse.ucdavis.edu>
2014-04-14 14:22:16 -0700

    Moved article data to external file.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
-  , app = express();
+  , app = express()
+  , ARTICLES = require('./data/articles.json');
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
@@ -7,27 +8,6 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('view options', {pretty: true});
 app.set('views', __dirname + '/views');
-
-var ARTICLES = [
-  {
-    id: 1,
-    title: 'How to write a JavaScript Framework',
-    author: 'Tomhuda Katzdale',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    id: 2,
-    title: 'Chronicles of an Embereño',
-    author: 'Alerik Bryneer',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    id: 3,
-    title: 'The Eyes of Thomas',
-    author: 'Yehuda Katz',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  }
-];
 
 var currentToken;
 app.post('/auth.json', function(req, res) {
