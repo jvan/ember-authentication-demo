82525b70355879fa6fb7c134a0cac2d8dbf9cabd
jvan <jvan@cse.ucdavis.edu>
2014-04-15 22:36:14 -0700

    Added some comments.

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,27 @@
+// Create the ember application.
 App = Ember.Application.create();
 
+// Setup routes.
 App.Router.map(function() {
+   // The articles route will display a list of all published articles.
+   // Individual articles can be reached by the nested route.
    this.resource('articles', function() {
       this.resource('articles.post', {path: '/:post_id'});
    });
+
+   // Add routes for administrators and logging in.
    this.route('admin');
    this.route('login');
 });
 
+// Configure the REST adapter to work with our API.
 DS.RESTAdapter.reopen({
    namespace: 'api'
 });
 
+// Create a data model for articles.
+//
+// NOTE: This data must match the data in articles.json.
 App.Article = DS.Model.extend({
    title:     DS.attr('string'),
    author:    DS.attr('string'),
@@ -19,10 +29,19 @@ App.Article = DS.Model.extend({
    body:      DS.attr('string')
 });
 
+
+// Include the login controller in the application controller. This allows
+// us to reference properties on the login controller in our application
+// templates.
 App.ApplicationController = Ember.Controller.extend({
    needs: ['login']
 });
 
+
+// Authenticate route base class.
+//
+// This route can be extended by routes which require authentication. If the
+// user is not currently logged in they will be redirected to the login page.
 App.AuthenticatedRoute = Ember.Route.extend({
    beforeModel: function(transition) {
       if (!this.controllerFor('login').get('token')) {
@@ -42,40 +61,63 @@ App.AuthenticatedRoute = Ember.Route.extend({
    }
 });
 
+
+// Main articles route.
+//
+// Display all published articles.
 App.ArticlesIndexRoute = App.AuthenticatedRoute.extend({
    model: function() {
       var token = this.controllerFor('login').get('token');
       var url = '/api/articles/published';
+
+      // NOTE: The built-in store.find(...) method is not compatible with
+      // the url for published articles. Instead we need to use the
+      // getJSON(...) function which returns a promise.
       return Ember.$.getJSON(url, {token: token}).then(function(data) {
-         return data.articles;
+         return data.articles; // Return the array of articles.
       });
    }
 });
 
+
+// Article detail route.
+//
+// Display data for a specific article.
 App.ArticlesPostRoute = App.AuthenticatedRoute.extend({
    model: function(params) {
       var token = this.controllerFor('login').get('token');
       var url = '/api/articles/'+params.post_id;
+
+      // NOTE: The store.find(...) method can take an additional id parameter
+      // for retrieving a specific item. However, it does not seem to work with
+      // the additional token data.
       return Ember.$.getJSON(url, {token: token}).then(function(data) {
          return data.article;
       });
    }
 });
 
+
+// Admin route.
+//
+// Display all articles (published and unpublished).
 App.AdminRoute = App.AuthenticatedRoute.extend({
    model: function() {
       return this.getJSONWithToken('article');
    }
 });
 
+
+// Login route and controller.
 App.LoginRoute = Ember.Route.extend({
+   // Reset the controller before rendering.
    setupController: function(controller, context) {
       controller.reset();
    }
 });
 
 App.LoginController = Ember.Controller.extend({
-
+   // Clear out any data from previous login attempts.
    reset: function() {
       this.setProperties({
          username: "",
@@ -84,12 +126,16 @@ App.LoginController = Ember.Controller.extend({
       });
    },
 
+   // Initialize user token using value in sessionStorage. This value will
+   // persist until the tab is closed (or logout).
    token: sessionStorage.token,
+
+   isLoggedIn: this.token !== null,
 
    isAdmin: false,
 
-   isLoggedIn: sessionStorage.getItem('token') !== null,
-
+   // When the token value is changed, save the value in sessionStorage and
+   // update the isLoggedIn property.
    tokenChanged: function() {
       if (this.get('token') === null) {
          delete sessionStorage.token;
@@ -101,16 +147,25 @@ App.LoginController = Ember.Controller.extend({
    }.observes('token'),
     
    actions: {
+      // Attempt to authenticate the user.
+      //
+      // If the login attempt is successful and the user was redirected to the
+      // login page they will be returned to the original page. Otherwise, they
+      // will be sent to the main page.
       login: function() {
          var self = this
            , data = this.getProperties('username', 'password');
 
          $.post('/api/auth.json', data).then(function(response) {
             self.set('errorMessage', response.message);
+
             if (response.success) {
+               // Store user token and set admin statuse.
                self.set('token', response.token);
                self.set('isAdmin', response.admin);
 
+               // Redirect to original page if the user was redirected to
+               // login page.
                var attemptedTransition = self.get('attemptedTransition');
                if (attemptedTransition) {
                   attemptedTransition.retry();
@@ -122,6 +177,7 @@ App.LoginController = Ember.Controller.extend({
          });
       },
 
+      // Log the user out and redirect to the main page.
       logout: function() {
          this.set('token', null);
          this.set('isAdmin', null);
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Test server for ember authentication demo.
+
 var express = require('express')
   , app = express()
   , _ = require('underscore')
@@ -7,29 +9,53 @@ var express = require('express')
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
+// Use jade templates for ember application html.
 app.set('view engine', 'jade');
 app.set('view options', {pretty: true});
 app.set('views', __dirname + '/views');
 
+
+// When users successfully login their tokens are stored along with
+// their access level (admin, regular user). These tokens and
+// associated data are used to restrict access to REST API defined
+// below.
 var currentTokens = { };
 
+// Return a "random" token for tracking logged in users.
+//
+// This is used to approximate the behavior of an actual login server
+// which would return a token upon successful login. This token is then
+// used in subsequent calls to the REST API.
 function generate_token() {
    return 'token-' + Object.keys(currentTokens).length;
 }
 
+// Get the user token from request object.
 function getUserToken(req, res) {
    return req.body.token || req.param('token') || req.headers.token;
 }
 
+// Return true if the users token is found.
 function validToken(userToken) {
    return currentTokens[userToken] !== undefined;
 } 
 
+// Return true if the user has admin status.
 function hasAdmin(userToken) {
    return currentTokens[userToken].admin === true;
 }
 
 
+// Data REST API
+//
+// These routes are used to authenticate users and retrieve article
+// data. In a real system these would belong to a separate backend.
+
+// Authenticate with username and password.
+//
+// To login to the system the user supplies a username and password.
+// If the attempt is successful a token is generated and returned to
+// the user. This token is then used in all future API calls.
 app.post('/api/auth.json', function(req, res) {
    var body = req.body,
        username = body.username,
@@ -37,12 +63,15 @@ app.post('/api/auth.json', function(req, res) {
 
    if (users[username] && users[username].password == password) {
       token = generate_token();
+
+      // Return the user token and the admin status in the response.
       res.send({
          success: true,
          token: token,
          admin: users[username].admin
       });
 
+      // Store the token and admin status.
       currentTokens[token] = { 'admin': users[username].admin };
    } else {
       res.send({
@@ -53,6 +82,7 @@ app.post('/api/auth.json', function(req, res) {
 });
 
 
+// Get all articles. This function requires admin status.
 app.get('/api/articles', function(req, res) {
    var userToken = getUserToken(req, res);
 
@@ -63,22 +93,34 @@ app.get('/api/articles', function(req, res) {
    }
 });
 
+
+// Get all published articles.
 app.get('/api/articles/published', function(req, res) {
    var userToken = getUserToken(req, res);
 
    if (validToken(userToken)) {
       var published = _(articles.articles).filter(function(x) { return x.published; });
+
+      // Format data for use with ember RESTAdapter.
       res.send({"articles": published});
    }
 });
 
+
+// Return an articles by id.
+//
+// If the article is published a regular, authenticated user can access the data.
+// Admin status is required to access unpublished articles.
 app.get('/api/articles/:article_id', function(req, res) {
    var userToken = getUserToken(req, res);
 
    if (validToken(userToken)) {
+      // Get the requested article.
       var index = parseInt(req.params.article_id); 
       var article = articles.articles[index];
+
       if (article.published || hasAdmin(userToken)) {
+         // Format data for use with ember RESTAdapter.
          res.send({"article": articles.articles[index]});
       } else {
          res.send(403, {error: 'You do not have permission to access this URL.' + userToken});
@@ -86,6 +128,10 @@ app.get('/api/articles/:article_id', function(req, res) {
    }
 });
 
+
+// Ember application.
+//
+// The ember application is served at localhost:3000.
 
 app.get('/', function(req, res) {
    res.render('layout');
